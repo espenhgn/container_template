6a52df40d8e724d78931176c50b49f94bf020a67
Espen Hagen <2492641+espenhgn@users.noreply.github.com>
2022-10-18 11:26:29 +0200

    revise tests + test script w. docker

--- tests/test_container_template.py
+++ tests/test_container_template.py
@@ -9,8 +9,14 @@ back to ``docker``.
 """
 
 import os
+import socket
 import subprocess
 
+
+# port used by tests
+sock = socket.socket()
+sock.bind(('', 0))
+port = sock.getsockname()[1]
 
 # Check that (1) singularity exist, and (2) if not, check for docker. 
 # If neither are found, tests will not run.
@@ -21,7 +27,10 @@ try:
 except FileNotFoundError:    
     try:
         out = subprocess.run('docker')
-        PREFIX = 'docker run -p 5001:5001 container_template'
+        pwd = os.getcwd()
+        PREFIX = f'docker run -p {port}:{port} container_template'
+        PREFIX_MOUNT = (f'docker run -p {port}:{port} ' + 
+            f'--mount type=bind,source={pwd},target={pwd} container_template')
     except FileNotFoundError:
         raise FileNotFoundError('Neither `singularity` nor `docker` found in PATH. Can not run tests!')
 
@@ -33,4 +42,10 @@ def test_container_template_python():
     """test that the Python installation works"""
     call = f'{PREFIX} python --version'
     out = subprocess.run(call.split(' '))
+    assert out.returncode == 0
+
+def test_container_template_python_script():
+    pwd = os.getcwd()
+    call = f'''{PREFIX_MOUNT} python {pwd}/tests/extras/hello.py'''
+    out = subprocess.run(call.split(' '), capture_output=True)
     assert out.returncode == 0
