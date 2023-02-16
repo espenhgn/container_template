18e17cd41610895cced7f109bb3f5cbbddd90d2d
Espen Hagen <2492641+espenhgn@users.noreply.github.com>
2023-02-16 20:11:44 +0100

    set up mambaforge for python stuff (#27)
    * set up mambaforge for python stuff
    
    * linting

--- src/scripts/apt_get_essential.sh
+++ src/scripts/apt_get_essential.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-apt-get update && apt-get install -y  --no-install-recommends python3=3.8.2-0ubuntu2 && \
+apt-get update && apt-get install -y  --no-install-recommends \
+    apt-utils=2.0.9 \
+    ca-certificates=20211016ubuntu0.20.04.1 \
+    curl=7.68.0-1ubuntu2.15 \
+    && \
+    update-ca-certificates && \
     apt-get clean && \
     rm -rf /var/lib/apt/lists/*
-
-update-alternatives --install /usr/bin/python python /usr/bin/python3 10
---
+++ src/scripts/install_mambaforge.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+version=22.11.1-4
+curl -sSL https://github.com/conda-forge/miniforge/releases/download/$version/Mambaforge-$version-$(uname)-$(uname -m).sh -o /tmp/mambaforge.sh \
+  && mkdir /root/.conda \
+  && bash /tmp/mambaforge.sh -bfp /usr/local \
+  && rm -rf /tmp/mambaforge.sh
+
+export PATH=$PATH:/opt/conda/bin
