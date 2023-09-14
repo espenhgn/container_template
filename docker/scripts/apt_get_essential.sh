03d434bb8ee500a8f5c603d0d8434b1ebd866d76
Espen Hagen <2492641+espenhgn@users.noreply.github.com>
2023-09-14 13:53:49 +0200

    Fix text example in Makefile, etc. (#36)

--- docker/scripts/apt_get_essential.sh
+++ docker/scripts/apt_get_essential.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+set -eou pipefail
+
 apt-get update && apt-get install -y  --no-install-recommends \
     apt-utils=2.0.9 \
-    ca-certificates=20211016ubuntu0.20.04.1 \
-    curl=7.68.0-1ubuntu2.15 \
+    ca-certificates=20230311ubuntu0.20.04.1 \
+    curl=7.68.0-1ubuntu2.19 \
     && \
     update-ca-certificates && \
     apt-get clean && \
